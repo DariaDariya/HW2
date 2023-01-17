87cc15c8c576a556743ae89d96c1b4425c9f9d94
Daria <dashik_gem@icloud.com>
2023-01-17 23:32:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 15000; // Стоимость авиабилета
+        int b = 20; // за какую сумму начисляется 1 балл
+        int bonus = a / b;
+
+
+
+        System.out.println("количество начисленных миль: " + bonus);
+    }
+}
